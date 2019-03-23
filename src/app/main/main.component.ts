d6ebbf62afbf73437333b6bf76c8c68d401b576a
koop4 <dilillo.roberto@gmail.com>
2019-03-23 20:17:00 +0100

    add cursor pointer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { FormsModule } from '@angular/forms';
 import AOS from 'aos';
-
-
-
-
-
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-
 
 export class MainComponent implements OnInit {
 
