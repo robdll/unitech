1f6daf26b7e32cf83c8435f1d30753175904ae7b
koop4 <dilillo.roberto@gmail.com>
2019-03-30 12:57:02 +0100

    style update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,19 @@ import AOS from 'aos';
 export class AppComponent implements OnInit {
   title = 'unipro';
   skillcategory: object = {
-    title: `<strong>Sviluppatori del Futuro</strong><br>Chi sono`,
+    title: `<strong>Sviluppatori del Futuro</strong>  -  Chi sono`,
     copy: `UniversitàTech è l'academy degli sviluppatori del futuro.<br>
     La nostra missione è forgiare esperienze e  conoscenza necessarie ad avviare la 
       <strong>TUA</strong> carriera nel mondo informatico.`
   };
   training: object = {
-    title: `<strong>Sviluppatori del Futuro</strong><br>Come si diventa`,
+    title: `<strong>Sviluppatori del Futuro</strong>  -  Come si diventa`,
     copy: `UniversitàTech è l'academy degli sviluppatori del futuro.<br>
     La nostra missione è forgiare esperienze e  conoscenza necessarie ad avviare la 
       <strong>TUA</strong> carriera nel mondo informatico.`
   };
   academy: object = {
-    title: `<strong>Universita' Tech</strong><br>La Nostra Missione`,
+    title: `<strong>Universita' Tech</strong>  -  La Nostra Missione`,
     copy: `Università.Tech ha l'obiettivo di trasformare sviluppatori junior in veri professionisti informatici fornendo la formazione;
     effettuando analisi degli interessi; sopponendo test attitudinali; e valutando l'abilità tecnica.
     Utilizzando queste informazioni indirizziamo i nostri candidati al ramo tecnico più adatto a loro, 
