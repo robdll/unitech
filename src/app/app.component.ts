3e72145c94418160831cc17ae4a9cc4e77311be6
koop4 <dilillo.roberto@gmail.com>
2019-03-30 10:12:34 +0100

    fix subscribe layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,23 @@ import AOS from 'aos';
 export class AppComponent implements OnInit {
   title = 'unipro';
   skillcategory: object = {
-    title: `asd`,
+    title: `<strong>Sviluppatori del Futuro</strong><br>Chi sono`,
     copy: `UniversitàTech è l'academy degli sviluppatori del futuro.<br>
     La nostra missione è forgiare esperienze e  conoscenza necessarie ad avviare la 
       <strong>TUA</strong> carriera nel mondo informatico.`
+  };
+  training: object = {
+    title: `<strong>Sviluppatori del Futuro</strong><br>Come si diventa`,
+    copy: `UniversitàTech è l'academy degli sviluppatori del futuro.<br>
+    La nostra missione è forgiare esperienze e  conoscenza necessarie ad avviare la 
+      <strong>TUA</strong> carriera nel mondo informatico.`
+  };
+  academy: object = {
+    title: `<strong>Universita' Tech</strong><br>La Nostra Missione`,
+    copy: `Università.Tech ha l'obiettivo di trasformare sviluppatori junior in veri professionisti informatici fornendo la formazione;
+    effettuando analisi degli interessi; sopponendo test attitudinali; e valutando l'abilità tecnica.
+    Utilizzando queste informazioni indirizziamo i nostri candidati al ramo tecnico più adatto a loro, 
+    preoccupandoci anche dell'inizio della loro carriera.`
   };
 
   ngOnInit() {
